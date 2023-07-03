138cb6a052572e327316484cb30b41ad11fcd3e5
zygoballus <119077481+zygoballus@users.noreply.github.com>
2023-07-03 13:23:17 -0400

    Add Traublookup API

---
+++ fleas/db.conf.sample.php
@@ -0,0 +1,5 @@
+<?php
+$dbname = "iz";
+$host = "127.0.0.1";
+$user = "user";
+$pass = "password";
---
+++ fleas/traublookupapi.php
@@ -0,0 +1,61 @@
+<?php
+// This script is dual licensed under the MIT License and the CC0 License.
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+
+/* Connect to the database. */
+include 'db.conf.php';
+$link = mysqli_connect($host, $user, $pass, $dbname);
+
+$strErrorDesc = '';
+
+function sendOutput( $data, $httpHeaders=array() ) {
+	header_remove('Set-Cookie');
+	if ( is_array($httpHeaders) && count($httpHeaders) ) {
+		foreach ( $httpHeaders as $httpHeader ) {
+			header( $httpHeader );
+		}
+	}
+	echo $data;
+	exit;
+}
+
+function getFlea( $abbrev ) {
+	global $link, $strErrorDesc;
+	$flea = NULL;
+	$result = mysqli_query( $link, "SELECT * FROM `traubabbrv` WHERE `abbrev` LIKE '".$abbrev."' LIMIT 1;" );
+	if ( $result ) {
+		$row = mysqli_fetch_array( $result );
+		if ( $row ) {
+			$flea['abbrev'] = $row['abbrev'];
+			$flea['name'] = $row['name'];
+			$flea['authority'] = $row['authority'];
+			$flea['notes'] = $row['notes'];
+			$flea['status'] = $row['status'];
+		}
+	} else {
+		$strErrorDesc = "Database query failed.";
+	}
+	return $flea;
+}
+
+parse_str( $_SERVER['QUERY_STRING'], $query );
+
+if ( isset( $query['abbrev'] ) ) {
+	$responseData = json_encode( getFlea( $query['abbrev'] ) );
+} else {
+	$strErrorDesc = "No abbreviation specified.";
+}
+
+// Send output
+if ( !$strErrorDesc ) {
+	sendOutput(
+		$responseData,
+		array( 'Content-Type: application/json', 'HTTP/1.1 200 OK' )
+	);
+} else {
+	sendOutput( 
+		json_encode( array( 'error' => $strErrorDesc ) ),
+		array( 'Content-Type: application/json', 'HTTP/1.1 500 Internal Server Error' )
+	);
+}
